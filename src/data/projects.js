11828119c189d2ba8ed177aedbdecaef0bf10c9e
Juancho7 <juanfertrom@gmail.com>
2024-01-02 20:36:46 -0500

    some changes on skills and projects were done

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,11 +1,17 @@
 const projects = [
-    {
-      projectTitle: 'This Portfolio',
-      repository: 'https://github.com/Juancho7/juan-my-portfolio',
-      img: 'https://raw.githubusercontent.com/Juancho7/juan-my-portfolio/master/docs/screenshot.JPG',
-      technologies: ['Astro', 'JavaScript', 'TailwindCSS', 'HTML'],
-    },
-    {
+  {
+    projectTitle: 'This Portfolio',
+    repository: 'https://github.com/Juancho7/juan-my-portfolio',
+    img: 'https://raw.githubusercontent.com/Juancho7/juan-my-portfolio/master/docs/screenshot.JPG',
+    technologies: ['Astro', 'JavaScript', 'TailwindCSS', 'HTML'],
+  },
+  {
+    projectTitle: 'Tasks CRUD',
+    repository: 'https://github.com/Juancho7/tasks-crud-react',
+    img: 'https://github.com/Juancho7/tasks-crud-react/blob/master/docs/screenshot.PNG?raw=true',
+    technologies: ['React', 'JavaScript', 'TailwindCSS', 'HTML'],
+  },
+  /* {
       projectTitle: 'Contacts Crud With Flask',
       repository: 'https://github.com/Juancho7/contacts-crud-flask',
       img: 'https://raw.githubusercontent.com/Juancho7/contacts-crud-flask/master/docs/Screenshot.JPG',
@@ -22,55 +28,55 @@ const projects = [
       repository: 'https://github.com/Juancho7/login_flask_mysql',
       img: '',
       technologies: ['Flask', 'Python', 'MySQL', 'HTML'],
-    },
-    {
-      projectTitle: 'Coding Challenges 2022',
-      repository: 'https://github.com/Juancho7/coding_challenges_2022',
-      img: '',
-      technologies: ['Python'],
-    },
-    {
-      projectTitle: '5 JavaScript Vanilla Projects',
-      repository: 'https://github.com/Juancho7/javascript-vanilla-projects',
-      img: '',
-      technologies: ['JavaScript', 'HTML', 'CSS'],
-    },
-    {
-      projectTitle: 'API Rest with FastAPI',
-      repository: 'https://github.com/Juancho7/apirest_fastapi',
-      img: '',
-      technologies: ['FastAPI', 'Python'],
-    },
-    {
-      projectTitle: '5 Basic Python Projects',
-      repository: 'https://github.com/Juancho7/5-basic-python-projects',
-      img: '',
-      technologies: ['Python'],
-    },
-    {
-      projectTitle: 'Basic To Do App With Tkinder',
-      repository: 'https://github.com/Juancho7/todoapp_tkinder',
-      img: '',
-      technologies: ['Python', 'Tkinder'],
-    },
-    {
-      projectTitle: 'Login With FastAPI and SQLite3',
-      repository: 'https://github.com/Juancho7/login_fastapi_sqlite3',
-      img: '',
-      technologies: ['FastAPI', 'SQLite3', 'Python'],
-    },
-    {
-      projectTitle: 'Todo App With React',
-      repository: 'https://github.com/Juancho7/todo_react_localstorage',
-      img: '',
-      technologies: ['React', 'TailwindCSS', 'JavaScript'],
-    },
-    {
-      projectTitle: 'Tesla Landing Page Clon',
-      repository: 'https://github.com/Juancho7/tesla-landing-page-clon',
-      img: '',
-      technologies: ['Astro', 'TailwindCSS', 'JavaScript', 'HTML', 'CSS'],
-    },
-  ];
+    }, */
+  {
+    projectTitle: 'Coding Challenges 2022',
+    repository: 'https://github.com/Juancho7/coding_challenges_2022',
+    img: '',
+    technologies: ['Python'],
+  },
+  {
+    projectTitle: '5 JavaScript Vanilla Projects',
+    repository: 'https://github.com/Juancho7/javascript-vanilla-projects',
+    img: '',
+    technologies: ['JavaScript', 'HTML', 'CSS'],
+  },
+  /* {
+    projectTitle: 'API Rest with FastAPI',
+    repository: 'https://github.com/Juancho7/apirest_fastapi',
+    img: '',
+    technologies: ['FastAPI', 'Python'],
+  }, */
+  {
+    projectTitle: '5 Basic Python Projects',
+    repository: 'https://github.com/Juancho7/5-basic-python-projects',
+    img: '',
+    technologies: ['Python'],
+  },
+  /* {
+    projectTitle: 'Basic To Do App With Tkinder',
+    repository: 'https://github.com/Juancho7/todoapp_tkinder',
+    img: '',
+    technologies: ['Python', 'Tkinder'],
+  }, 
+  {
+    projectTitle: 'Login With FastAPI and SQLite3',
+    repository: 'https://github.com/Juancho7/login_fastapi_sqlite3',
+    img: '',
+    technologies: ['FastAPI', 'SQLite3', 'Python'],
+  }, */
+  {
+    projectTitle: 'Todo App With React',
+    repository: 'https://github.com/Juancho7/todo_react_localstorage',
+    img: 'https://github.com/Juancho7/todo_react_localstorage/blob/master/docs/screenshot.PNG?raw=true',
+    technologies: ['React', 'TailwindCSS', 'JavaScript'],
+  },
+  {
+    projectTitle: 'Tesla Landing Page Clon',
+    repository: 'https://github.com/Juancho7/tesla-landing-page-clon',
+    img: 'https://github.com/Juancho7/tesla-landing-page-clon/blob/master/docs/screenshot.PNG?raw=true',
+    technologies: ['Astro', 'TailwindCSS', 'JavaScript', 'HTML', 'CSS'],
+  },
+];
 
-export default projects
+export default projects;
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,10 +1,25 @@
 const skills = [
   {
+    skillName: 'JavaScript',
+    logo: './javascript_icon.svg',
+    colors: 'text-yellow-900 bg-yellow-300',
+  },
+  {
+    skillName: 'Astro',
+    logo: './astro_icon.svg',
+    colors: 'text-orange-50 bg-orange-700',
+  },
+  {
+    skillName: 'React',
+    logo: './react_icon.svg',
+    colors: 'text-blue-100 bg-blue-500',
+  },
+  {
     skillName: 'Python',
     logo: './python_icon.svg',
     colors: 'text-yellow-800 bg-yellow-200',
   },
-  {
+  /* {
     skillName: 'FastAPI',
     logo: './fastapi_icon.svg',
     colors: 'text-green-800 bg-green-200',
@@ -18,7 +33,7 @@ const skills = [
     skillName: 'Flask',
     logo: './flask_icon.svg',
     colors: 'text-green-800 bg-green-200',
-  },
+  }, */
   {
     skillName: 'SQL',
     logo: './sql_icon.svg',
@@ -45,21 +60,6 @@ const skills = [
     colors: 'text-sky-700 bg-sky-200',
   },
   {
-    skillName: 'JavaScript',
-    logo: './javascript_icon.svg',
-    colors: 'text-yellow-900 bg-yellow-300',
-  },
-  {
-    skillName: 'Astro',
-    logo: './astro_icon.svg',
-    colors: 'text-orange-50 bg-orange-700',
-  },
-  {
-    skillName: 'React',
-    logo: './react_icon.svg',
-    colors: 'text-blue-100 bg-blue-500',
-  },
-  {
     skillName: 'Git',
     logo: './git_icon.svg',
     colors: 'text-red-800 bg-red-200',
@@ -69,11 +69,11 @@ const skills = [
     logo: './github_icon.svg',
     colors: 'text-gray-800 bg-gray-200',
   },
-  {
+  /* {
     skillName: 'RegEx',
     logo: './regex_icon.svg',
     colors: 'text-neutral-900 bg-neutral-100',
-  },
+  }, */
   {
     skillName: 'VSCode',
     logo: './vscode_icon.svg',
