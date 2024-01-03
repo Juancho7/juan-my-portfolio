fccbfcf199a4453b80b5d73d659287e221dd9c67
Juancho7 <juanfertrom@gmail.com>
2024-01-02 22:14:51 -0500

    certificates page is now avaliable

---
+++ src/data/certificates.js
@@ -0,0 +1,34 @@
+const certificates = [
+  {
+    title: 'HTML5 desde cero Edición 2020',
+    urlToVisit:
+      'https://www.udemy.com/certificate/UC-dda7abe1-b50c-476d-8197-c9f796288719/',
+  },
+  {
+    title: 'Desarrollo web. CSS desde cero. Edición 2020',
+    urlToVisit:
+      'https://www.udemy.com/certificate/UC-a6d46140-94a0-4175-98cb-c8354d25416d/',
+  },
+  {
+    title: 'Universidad Desarrollo Web - FrontEnd Web Developer!',
+    urlToVisit:
+      'https://www.udemy.com/certificate/UC-41afc6be-ac3d-419a-aa77-2cb9a0b60c07/',
+  },
+  {
+    title: 'Tailwind CSS: Fundamentos',
+    urlToVisit:
+      'https://www.udemy.com/certificate/UC-bcc7bcfb-4ce1-4d97-8277-37e9d16dc689/',
+  },
+  {
+    title: 'Comienza a programar: Python desde 0',
+    urlToVisit:
+      'https://www.udemy.com/certificate/UC-0d4915d9-8990-4723-8ded-451154292d4e/',
+  },
+  {
+    title: 'Javascript Practicals Crash Course',
+    urlToVisit:
+      'https://www.udemy.com/certificate/UC-87f19f35-1d26-4200-a9e5-dd60c4fddc1a/',
+  },
+];
+
+export default certificates;
